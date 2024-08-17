7c5a8d7f03e344b9bf64b52e292190526aa4cb28
DanielZerihunGeda <lenjisa48@gmail.com>
2024-08-17 09:37:57 +0300

    log environment-1

--- server/config/config.js
+++ server/config/config.js
@@ -15,4 +15,18 @@ module.exports = {
       }
     }
   },
+  production: {
+    username: process.env.DB_USERNAME,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_NAME,
+    host: process.env.DB_HOST,
+    port: process.env.DB_PORT,
+    dialect: process.env.DB_DIALECT || 'postgres',
+    dialectOptions: {
+      ssl: {
+        require: true,
+        rejectUnauthorized: false // set to true if needed
+      }
+    }
+  }
 };
